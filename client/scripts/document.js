db913cbb0f5cacd371b27ed2df468bee3bfa071b
Michelle C. Funk <michellefunk@exosite.com>
2016-01-15 09:31:14 -0600

    fix routing for start.html

--- client/scripts/document.js
+++ client/scripts/document.js
@@ -1,56 +1,32 @@
 $(document).ready(function() {
-    $.get("/grav", {name:'michelle@michellefunk.com'}, function(gravatarUrl) {
-        $.ajax({
-                type: 'GET',
-                dataType: 'jsonp',
-                crossDomain: true,
-                url: gravatarUrl,
-                complete: function() {
-                    console.log("Ajax call complete")
-                },
-                success: function(data) {
-                    var profile = data.entry[0];
-                    console.log(data);
-                    $(".gravatar").append("<a href='" + profile.profileUrl + "'> " +
-                        "<img class='img-bordered img-responsive' src='" + profile.photos[0].value + "?s=150&d=identicon&r=G'></a>");
-                    $(".gravatar").append("<h4 class='white'>" + profile.displayName + "<br></h5>");
-                    $(".gravatar").append("<h5><em>" + profile.currentLocation + "<br></em></h5>");
-                    $(".gravatar").append("<h5>" + profile.aboutMe + "</h5>");
-                },
-                error: function(xhr, status) {
-                    console.log("Error : ", xhr, " ", status);
-                }
-        });
-    });
-
-    $.get("/assets/scripts/profile.html", function(data) {
+    $.get("/assets/templates/start.html", function(data) {
         $(".textArea").html(data);
         setHeight();
     });
 
-    $("#profile").on("click", function() {
-        $.get("/assets/scripts/profile.html", function(data){
+    $("#create-device").on("click", function() {
+        $.get("/assets/templates/create-device.html", function(data){
             $(".textArea").html(data);
             setHeight();
         });
     });
 
-    $("#employment").on("click", function() {
-        $.get("/assets/scripts/employment.html", function(data){
+    $("#create-datasource").on("click", function() {
+        $.get("/assets/templates/create-datasource.html", function(data){
             $(".textArea").html(data);
             setHeight();
         });
     });
 
-    $("#education").on("click", function() {
-        $.get("/assets/scripts/education.html", function(data){
+    $("#write-data").on("click", function() {
+        $.get("/assets/templates/write-data.html", function(data){
             $(".textArea").html(data);
             setHeight();
         });
     });
 
-    $("#community").on("click", function() {
-        $.get("/assets/scripts/community.html", function(data){
+    $("#create-script").on("click", function() {
+        $.get("/assets/scripts/script.html", function(data){
             $(".textArea").html(data);
             setHeight();
         });

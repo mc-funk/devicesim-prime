13fc57e99af2c6eac111535ff350210b3c478ed4
Michelle C. Funk <michellefunk@exosite.com>
2016-01-19 16:49:06 -0600

    giving process.env.MONGOLAB_URI a shot

--- server/config.js
+++ server/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   db: {
-    production: "mongodb://heroku_jktj79c9:t7bg9rqid896acev3gv15i5hs1@ds047325.mongolab.com:47325/heroku_jktj79c9",
+    production: process.env.MONGOLAB_URI,
     development: "mongodb://localhost:27017/sessions-dev",
     test: "mongodb://localhost:27017/sessions-test",
   }

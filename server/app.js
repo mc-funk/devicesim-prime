e55d0ec8676116f4b12f6b7a439f7d4543f50927
Michelle C. Funk <michellefunk@exosite.com>
2016-01-19 16:01:22 -0600

    sessions ostensibly now working with Mongoose

--- server/app.js
+++ server/app.js
@@ -3,24 +3,25 @@ var app = express();
 // var base64Uid = require('./user');
 
 var path = require("path");
-// var cookieParser = require('cookie-parser');
-// const session = require('express-session');
-// const MongoStore = require('connect-mongo')(session);
+var cookieParser = require('cookie-parser');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
 
 //var hash = require("./gravatar");
 
 app.set('port', (process.env.PORT || 5000)); //Heroku sets env PORT to 5000
 
-// app.use(cookieParser());
-// app.use(session({
-//   secret: 'DkTv37Ls',
-//   resave: true,
-//   saveUninitialized:true,
-//   store: new MongoStore({
-//     url: 'mongodb://localhost/27017',
-//     ttl: 14 * 24 * 60 * 60,
-//   })
-// }));
+app.use(cookieParser());
+app.use(session({
+  secret: 'DkTv37Ls',
+  resave: true,
+  saveUninitialized:true,
+  store: new MongoStore({
+    url: 'mongodb://localhost/27017',
+    mongoOptions: {},
+    ttl: 14 * 24 * 60 * 60
+  })
+}));
 
 app.get('/*', function(request, response) {
     var file = request.params[0] || 'views/index.html';
---
+++ server/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  db: {
+    production: "mongodb://user:pass@example.com:1234/sessions-prod",
+    development: "mongodb://localhost:27017/sessions-dev",
+    test: "mongodb://localhost:27017/sessions-test",
+  }
+};

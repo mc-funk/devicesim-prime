30adb551206a683cb80a6daf363fc1fb0fa24169
Michelle C. Funk <michellefunk@exosite.com>
2016-01-19 16:30:05 -0600

    trying different mongolabs auth

--- server/config.js
+++ server/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   db: {
-    production: "mongodb://michellefunk:DataExo9225@ds047325.mongolab.com:47325/heroku_jktj79c9",
+    production: "mongodb://heroku_jktj79c9:t7bg9rqid896acev3gv15i5hs1@ds047325.mongolab.com:47325/heroku_jktj79c9",
     development: "mongodb://localhost:27017/sessions-dev",
     test: "mongodb://localhost:27017/sessions-test",
   }

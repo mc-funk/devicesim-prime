5519ad99ab6864c23cd27c27ae488f716b0e496d
Michelle C. Funk <michellefunk@exosite.com>
2016-01-19 16:07:57 -0600

    Add MongoLab URI in config

--- server/config.js
+++ server/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   db: {
-    production: "mongodb://user:pass@example.com:1234/sessions-prod",
+    production: "mongodb://michellefunk:DataExo9225@ds047325.mongolab.com:47325/heroku_jktj79c9",
     development: "mongodb://localhost:27017/sessions-dev",
     test: "mongodb://localhost:27017/sessions-test",
   }

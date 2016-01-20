cde0f4ecf53e47159b1c8d44abd019ef739f5ff5
Michelle C. Funk <michellefunk@exosite.com>
2016-01-20 10:48:24 -0600

    app.configure removed (deprecated); code running with Mongoose again

--- server/app.js
+++ server/app.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var app = express();
+var config = require('./config');
+var mongoose = require('mongoose');
 // var base64Uid = require('./user');
 
-var path = require("path");
+var path = require('path');
 var cookieParser = require('cookie-parser');
 const session = require('express-session');
 const MongoStore = require('connect-mongo')(session);
@@ -11,13 +13,20 @@ const MongoStore = require('connect-mongo')(session);
 
 app.set('port', (process.env.PORT || 5000)); //Heroku sets env PORT to 5000
 
+// set the 'dbUrl' to the mongodb url that corresponds to the
+// environment we are in
+app.set('dbUrl', process.env.MONGOLAB_URI || config.db[app.settings.env]);
+// connect mongoose to the mongo dbUrl
+mongoose.connect(app.get('dbUrl'));
+//...
+
 app.use(cookieParser());
 app.use(session({
   secret: 'DkTv37Ls',
   resave: true,
   saveUninitialized:true,
   store: new MongoStore({
-    url: 'mongodb://localhost/27017',
+    mongooseConnection: mongoose.connection,
     mongoOptions: {},
     ttl: 14 * 24 * 60 * 60
   })
